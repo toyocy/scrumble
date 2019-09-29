5b918a280dd28ab17dcaad7a02f93967e7822b51
Taka, Toyoshima <oq0q6383qqs@gmail.com>
2019-09-29 21:59:50 +0900

    migrated task model

--- db/migrate/20190929125733_create_tasks.rb
+++ db/migrate/20190929125733_create_tasks.rb
@@ -1,12 +1,12 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :name
-      t.text :description
+      t.string :name, null: false, default: ''
+      t.text :description, default: ''
       t.datetime :started_at
       t.datetime :finished_at
-      t.boolean :is_allday
-      t.boolean :is_done
+      t.boolean :is_allday, default: false
+      t.boolean :is_done, default: false
 
       t.timestamps
     end
